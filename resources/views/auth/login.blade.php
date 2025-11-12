3ef08758414b327b3a1e9ac2d47309395f22a400
Daffa <daffapermana885@gmail.com>
2025-11-12 10:55:57 +0700

    landing page dan register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Absensi & Aktivitas Magang</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body class="bg-gray-50 text-gray-800">
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register - Absensi & Aktivitas Magang</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 </head>
 <body class="bg-gray-50 text-gray-800">
 
